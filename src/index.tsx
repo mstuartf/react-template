7b78fcae517012392c1a77e8ecacd06dfe7ff505
mikef <mstuartflood@gmail.com>
2020-10-05 10:34:00 +0100

    add HMR

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,19 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/tailwind.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+// wrap in a render method to support hot module reloading
+const render = (Component: any) => {
+  return ReactDOM.render(<Component />, document.getElementById("root"));
+};
+
+render(App);
+
+if ((module as any).hot) {
+  (module as any).hot.accept("./App", () => {
+    const NextApp = require("./App").default;
+    render(NextApp);
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
